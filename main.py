ce3d19427edf2aea8a54c20fc25377f9af629c90
Joshua Beitler <joshbeitler@gmail.com>
2014-01-18 11:50:22 -0700

    Added some content to main

---
+++ main.py
@@ -0,0 +1,12 @@
+# Import modules
+import pygame
+from pygame.locals import *
+
+game_size = (500, 500)
+
+def main():
+    pygame.init()
+    display_surface = pygame.display.set_mode(game_size)
+
+if __name__ == "__main__":
+    main()
