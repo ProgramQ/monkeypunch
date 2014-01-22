3b8894a579f896bf6c51e63a9674d94d5e28c72f
Brandon Bench <benchlord@gmail.com>
2014-01-21 21:45:36 -0700

    shop is a go (sort of)!

---
+++ source/shop.py
@@ -0,0 +1,6 @@
+import score
+import fist # that sounds nasty
+
+# were going to have to have to loops, one for the gameplay,
+# and one for the shop. If you set up the skeleton for the shop
+# I'll do everything else!
